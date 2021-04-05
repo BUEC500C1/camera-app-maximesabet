40c016520eb3c01de609e632748be03a6c37a4bc
maximesabet <maximesabetdacre@gmail.com>
2021-04-04 21:08:52 -0400

    Added authentification pages login sign up log out using online tutorial

--- cameraapp/App.js
+++ cameraapp/App.js
@@ -30,79 +30,102 @@
 //
 //
 
-import * as React from 'react';
-import {Button, View, Text, Platform, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import MapView from "react-native-maps";
 
-function HomeScreen({navigation}) {
+//map and navigation
+
+
+// import * as React from 'react';
+// import {Button, View, Text, Platform, StyleSheet} from 'react-native';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+// import MapView from "react-native-maps";
+
+// function HomeScreen({navigation}) {
+//   return (
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Go to Map"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//       <Button
+//         title="Go to something else"
+//         onPress={() => navigation.navigate('Details2')}
+//       />
+//     </View>
+//   );
+// }
+
+// function DetailsScreen({navigation}) {
+//     return (
+//       <MapView
+//         style={styles.map}
+//         initialRegion={{
+//           latitude: 37.78825,
+//           longitude: -122.4324,
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421,
+//         }}
+//       />
+//     );
+//   }
+
+// function DetailsScreen2({navigation}) {
+//   return (
+//     <MapView
+//       style={styles.map}
+//       initialRegion={{
+//         latitude: 37.78825,
+//         longitude: -122.4324,
+//         latitudeDelta: 0.0922,
+//         longitudeDelta: 0.0421,
+//       }}
+//     />
+//   );
+// }
+
+
+// const Stack = createStackNavigator();
+
+// const styles = StyleSheet.create({
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+// });
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           options={{title: 'Overview'}}
+//         />
+//         <Stack.Screen name="Details" component={DetailsScreen} />
+//         <Stack.Screen name="Details2" component={DetailsScreen2} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
+
+
+//
+//
+//
+//
+
+//Authentification
+
+import React from 'react';
+import {AuthProvider} from './src/navigation/AuthProvider';
+import Routes from './src/navigation/Routes';
+export default function Providers() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Map"
-        onPress={() => navigation.navigate('Details')}
-      />
-      <Button
-        title="Go to something else"
-        onPress={() => navigation.navigate('Details2')}
-      />
-    </View>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
-
-function DetailsScreen({navigation}) {
-    return (
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    );
-  }
-
-function DetailsScreen2({navigation}) {
-  return (
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  );
-}
-
-
-const Stack = createStackNavigator();
-
-const styles = StyleSheet.create({
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Overview'}}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Details2" component={DetailsScreen2} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
